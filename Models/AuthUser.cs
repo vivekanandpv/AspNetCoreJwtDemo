da5d86ed5738285c206c0e7cf4092b7f96896a42
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-08 23:08:08 +0530

    done

---
+++ Models/AuthUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AspNetCoreJwtDemo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+
+    public class UserRole
+    {
+        public int UserId { get; set; }
+        
+        [JsonIgnore]
+        public User User { get; set; }
+
+        public int RoleId { get; set; }
+
+        [JsonIgnore]
+        public Role Role { get; set; }
+    }
+}
